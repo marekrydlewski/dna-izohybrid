3b6260c023e03b237af6935110731f70a09886bd
Marek Rydlewski <marek.rydlewski@outlook.com>
2016-05-19 22:48:28 +0200

    Creating a map with scattered dna

--- dna_oligonucleotides.hpp
+++ dna_oligonucleotides.hpp
@@ -2,17 +2,23 @@
 
 #include <iostream>
 #include <string>
+#include <map>
 
 class DnaOligonucleotides
 {
 private:
     const static int shift{ 2 };
+    const static int atTemp{ 2 };
+    const static int cgTemp{ 4 };
     std::string dna;
     std::map<std::string, int> oligoMap;
     int temp;
     int temp2;
+    void generateScatteredMap();
+    int getNucleotideTemp(char);
 public:
     DnaOligonucleotides(std::string dna, int temp);
+    std::map<std::string, int> getOligoMap() { return this->oligoMap; };
     virtual ~DnaOligonucleotides();
 };
 
--- main.cpp
+++ main.cpp
@@ -6,14 +6,19 @@
 
 const int K_MER_LENGTH = 5;
 const int DNA_LENGTH = 20;
+const int DNA_TEMPERATURE = 6;
 
 int main(int argc, const char * argv[]) {
+
     auto dna_loader = new DnaSequence("dna-data.txt");
-    auto dna_scattered = new DnaOligonucleotides(dna_loader->getDna(DNA_LENGTH), 10);
+    auto dna_scattered = new DnaOligonucleotides(dna_loader->getDna(DNA_LENGTH), DNA_TEMPERATURE);
+    auto oligo_map = dna_scattered->getOligoMap();
+    for (auto &t : oligo_map)
+        std::cout << t.first << " " << t.second << std::endl;
 
     Graph* graph = new Graph("a_long_long_long_time", K_MER_LENGTH);
     auto path = graph->eulerianPath();
-    for (auto item : path)
+    for (auto &item : path)
         printf("%s,", item->value().c_str());
     // output should be:
     // ['a_lo', '_lon', 'long', 'ong_', 'ng_l', 'g_lo', '_lon', 'long', 'ong_', 'ng_l',
