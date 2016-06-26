b365963a8c28b31dbd6f533855b1fcaeb077bfaa
borewicz <waren@outlook.com>
2016-06-26 16:17:42 +0200

    beta doc

--- main.cpp
+++ main.cpp
@@ -77,11 +77,11 @@ int main(int argc, const char * argv[])
 int main(int argc, const char* argv[])
 {
     std::vector<double> errors_ratio = {0.00, 0.5, 0.10, 0.15 };
-    std::vector<int> dna_length = { 20};
-    std::vector<int> dna_temp = {20, 30, 40};
+	std::vector<int> dna_length = { 25 };
+    std::vector<int> dna_temp = { 40};
     std::ofstream outfile;
     outfile.open("test.csv", std::ios::app);
-    outfile << "Length" << ";" << "Temp" << ";" << "Error" << ";" << "Time" << ";" << "Score" << "\n";
+    outfile << "Length" << ";" << "Temp" << ";" << "Error" << ";" << "Time" << ";";
     for (auto& length : dna_length)
     {
         for(auto& temp: dna_temp)
